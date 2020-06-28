8905be982e4d1f74c9724d18c7d17e16f27383c7
Joshua Rowe <josh@phantom.to>
2020-06-28 14:48:22 -0700

    Setting up project structure

---
+++ Phantom.ProtoStore/Phantom.ProtoStore.cpp
@@ -0,0 +1,12 @@
+﻿// Phantom.ProtoStore.cpp : Defines the entry point for the application.
+//
+
+#include "Phantom.ProtoStore.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
