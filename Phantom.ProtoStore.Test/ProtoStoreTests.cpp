fae3a54b36cca8f7d3687def028d65735800401c
Joshua Rowe <josh@phantom.to>
2020-07-02 17:12:45 -0700

    Added test TEST(ProtoStoreTests, CanOpen_memory_backed_store)

--- Phantom.ProtoStore.Test/ProtoStoreTests.cpp
+++ Phantom.ProtoStore.Test/ProtoStoreTests.cpp
@@ -19,4 +19,20 @@ TEST(ProtoStoreTests, CanCreate_memory_backed_store)
     });
 }
 
+TEST(ProtoStoreTests, CanOpen_memory_backed_store)
+{
+    run_async([]() -> task<>
+    {
+        auto storeFactory = MakeProtoStoreFactory();
+        CreateProtoStoreRequest createRequest;
+
+        createRequest.ExtentStore = UseMemoryExtentStore();
+
+        auto store = co_await storeFactory->Create(
+            createRequest);
+        store = co_await storeFactory->Open(
+            createRequest);
+    });
+}
+
 }
