3117920232efa4e62817d50fed18c09c9d525bc3
Joshua Rowe <j.git@phantom.to>
2023-02-07 13:46:11 -0800

    Fix up lambdas

--- Phantom.ProtoStore.Test/ProtoStoreTests.cpp
+++ Phantom.ProtoStore.Test/ProtoStoreTests.cpp
@@ -1191,6 +1191,9 @@ TEST_F(ProtoStoreTests, DISABLED_Can_commit_transaction)
     });
 }
 
+cppcoro::async_scope* m_perfScope;
+shared_ptr<IProtoStore>* m_perfStore;
+
 TEST_F(ProtoStoreTests, PerformanceTest(Perf1))
 {
     cppcoro::static_thread_pool threadPool(4);
@@ -1198,6 +1201,7 @@ TEST_F(ProtoStoreTests, PerformanceTest(Perf1))
     run_async([&]() -> task<>
     {
         auto store = co_await CreateMemoryStore();
+        m_perfStore = &store;
 
         auto index = co_await CreateTestIndex(
             store);
@@ -1288,6 +1292,7 @@ TEST_F(ProtoStoreTests, PerformanceTest(Perf1))
 }
 
 std::atomic<long> Perf2_running_items(0);
+std::vector<shared_task<>> performanceTasks;
 
 TEST_F(ProtoStoreTests, PerformanceTest(Perf2))
 {
@@ -1298,13 +1303,14 @@ TEST_F(ProtoStoreTests, PerformanceTest(Perf2))
         createRequest.Schedulers = Schedulers::Default();
 
 #ifdef NDEBUG
-        createRequest.CheckpointLogSize = 10000000;
+        createRequest.CheckpointLogSize = 1000000;
 #else
         createRequest.CheckpointLogSize = 1000;
 #endif
 
         auto store = co_await CreateStore(
             createRequest);
+        m_perfStore = &store;
 
         auto index = co_await CreateTestIndex(
             store);
@@ -1335,6 +1341,36 @@ TEST_F(ProtoStoreTests, PerformanceTest(Perf2))
         cppcoro::async_scope asyncScopeWrite;
         auto schedulers = Schedulers::Default();
 
+        auto writeItemLambda = [&](size_t startKeyIndex, size_t endKeyIndex) -> task<>
+        {
+            co_await schedulers.ComputeScheduler->schedule();
+            Perf2_running_items.fetch_add(1);
+
+            co_await store->ExecuteOperation(
+                BeginTransactionRequest(),
+                [&](IOperation* operation)->task<>
+            {
+                for (auto myKeyIndex = startKeyIndex;
+                myKeyIndex < endKeyIndex;
+                    myKeyIndex++)
+            {
+                auto& myKey = keys[myKeyIndex];
+                StringKey key;
+                key.set_value(myKey);
+                StringValue expectedValue;
+                expectedValue.set_value(myKey);
+
+                co_await operation->AddRow(
+                    WriteOperationMetadata{},
+                    index,
+                    &key,
+                    &expectedValue);
+            }
+            });
+
+            Perf2_running_items.fetch_sub(1);
+        };
+
         auto keysPerInsert = 20;
         for (size_t startKeyIndex = 0; startKeyIndex < keys.size(); startKeyIndex += keysPerInsert)
         {
@@ -1342,118 +1378,91 @@ TEST_F(ProtoStoreTests, PerformanceTest(Perf2))
                 startKeyIndex + keysPerInsert,
                 keys.size());
 
-            asyncScopeWrite.spawn([&](size_t startKeyIndex, size_t endKeyIndex) -> task<>
-            {
-                co_await schedulers.ComputeScheduler->schedule();
-                Perf2_running_items.fetch_add(1);
-
-                co_await store->ExecuteOperation(
-                    BeginTransactionRequest(),
-                    [&](IOperation* operation)->task<>
-                {
-                    for (auto myKeyIndex = startKeyIndex;
-                        myKeyIndex < endKeyIndex;
-                        myKeyIndex++)
-                    {
-                        auto& myKey = keys[myKeyIndex];
-                        StringKey key;
-                        key.set_value(myKey);
-                        StringValue expectedValue;
-                        expectedValue.set_value(myKey);
-
-                        co_await operation->AddRow(
-                            WriteOperationMetadata{},
-                            index,
-                            &key,
-                            &expectedValue);
-                    }
-                });
-
-                Perf2_running_items.fetch_sub(1);
-            }(startKeyIndex, endKeyIndex));
+            asyncScopeWrite.spawn(writeItemLambda(startKeyIndex, endKeyIndex));
         }
 
         co_await asyncScopeWrite.join();
 
-
         auto endWriteTime = chrono::high_resolution_clock::now();
-        auto readWriteRuntimeMs = chrono::duration_cast<chrono::milliseconds>(endWriteTime - startWriteTime);
+        auto writeRuntimeMs = chrono::duration_cast<chrono::milliseconds>(endWriteTime - startWriteTime);
 
-        std::cout << "ProtoStoreTests write runtime: " << readWriteRuntimeMs.count() << "\r\n" << std::flush;
+        std::cout << "ProtoStoreTests write runtime: " << writeRuntimeMs.count() << "ms\r\n" << std::flush;
 
-        auto readLambda = [&]() -> task<>
+        auto readLambda = [&]() -> shared_task<>
         {
             auto startReadTime = chrono::high_resolution_clock::now();
 
             Perf2_running_items.store(0);
 
-            std::vector<task<>> tasks;
-
 #ifdef NDEBUG
-            int threadCount = 50;
+            int threadCount = valueCount;
 #else
             int threadCount = 1;
 #endif
-            for (int threadNumber = 0; threadNumber < threadCount; threadNumber++)
+            auto readItemLambda = [&](int threadNumber) -> shared_task<>
             {
-                tasks.push_back([&](int threadNumber) -> task<>
+                co_await schedulers.ComputeScheduler->schedule();
+
+                auto endKeyIndex = std::min(
+                    keys.size() / threadCount * (threadNumber + 1) - 1,
+                    keys.size());
+
+                for (auto keyIndex = keys.size() / threadCount * threadNumber;
+                    keyIndex < endKeyIndex;
+                    keyIndex++)
                 {
-                    co_await schedulers.ComputeScheduler->schedule();
-
-                    auto endKeyIndex = std::min(
-                        keys.size() / threadCount * (threadNumber + 1) - 1,
-                        keys.size());
-
-                    for (auto keyIndex = keys.size() / threadCount * threadNumber;
-                        keyIndex < endKeyIndex;
-                        keyIndex++)
+                    if (keyIndex % 1000 == 0)
                     {
-                        if (keyIndex % 1000 == 0)
-                        {
-                            co_await *schedulers.ComputeScheduler;
-                        }
-
-                        auto myKey = keys[keyIndex];
-
-                        Perf2_running_items.fetch_add(1);
-
-                        StringKey key;
-                        key.set_value(myKey);
-                        StringValue expectedValue;
-                        expectedValue.set_value(myKey);
-
-                        ReadRequest readRequest;
-                        readRequest.Key = &key;
-                        readRequest.Index = index;
-
-                        auto readResult = co_await store->Read(
-                            readRequest
-                        );
-
-                        StringValue actualValue;
-                        readResult.Value.unpack(&actualValue);
-
-                        auto messageDifferencerResult = MessageDifferencer::Equals(
-                            expectedValue,
-                            actualValue);
-
-                        EXPECT_TRUE(messageDifferencerResult);
-
-                        Perf2_running_items.fetch_sub(1);
+                        co_await *schedulers.ComputeScheduler;
                     }
 
-                }(threadNumber));
+                    auto myKey = keys[keyIndex];
+
+                    Perf2_running_items.fetch_add(1);
+
+                    StringKey key;
+                    key.set_value(myKey);
+                    StringValue expectedValue;
+                    expectedValue.set_value(myKey);
+
+                    ReadRequest readRequest;
+                    readRequest.Key = &key;
+                    readRequest.Index = index;
+
+                    auto readResult = co_await store->Read(
+                        readRequest
+                    );
+
+                    StringValue actualValue;
+                    readResult.Value.unpack(&actualValue);
+
+                    auto messageDifferencerResult = MessageDifferencer::Equals(
+                        expectedValue,
+                        actualValue);
+
+                    EXPECT_TRUE(messageDifferencerResult);
+
+                    Perf2_running_items.fetch_sub(1);
+                }
+            };
+
+            cppcoro::async_scope asyncScopeRead;
+            m_perfScope = &asyncScopeRead;
+
+            for (int threadNumber = 0; threadNumber < threadCount; threadNumber++)
+            {
+                performanceTasks.push_back(
+                    readItemLambda(threadNumber));
+                asyncScopeRead.spawn(
+                    performanceTasks.back());
             }
 
-            for (auto& task : tasks)
-            {
-                co_await task;
-            }
+            co_await asyncScopeRead.join();
 
             auto endReadTime = chrono::high_resolution_clock::now();
             auto readRuntimeMs = chrono::duration_cast<chrono::milliseconds>(endReadTime - startReadTime);
 
-            std::cout << "ProtoStoreTests read runtime: " << readRuntimeMs.count() << "\r\n";
+            std::cout << "ProtoStoreTests read runtime: " << readRuntimeMs.count() << "ms\r\n";
         };
 
         co_await readLambda();
@@ -1465,64 +1474,64 @@ TEST_F(ProtoStoreTests, PerformanceTest(Perf2))
 
             Perf2_running_items.store(0);
 
-            std::vector<task<>> tasks;
-
 #ifdef NDEBUG
             int threadCount = 50;
 #else
             int threadCount = 1;
 #endif
-            for (int threadNumber = 0; threadNumber < threadCount; threadNumber++)
+            auto readItemsLambda = [&](int threadNumber) -> shared_task<>
             {
-                tasks.push_back([&](int threadNumber) -> task<>
+                auto endKeyIndex = std::min(
+                    keys.size() / threadCount * (threadNumber + 1) - 1,
+                    keys.size());
+
+                for (auto keyIndex = nonExistentKeys.size() / threadCount * threadNumber;
+                    keyIndex < endKeyIndex;
+                    keyIndex++)
                 {
-                    auto endKeyIndex = std::min(
-                        keys.size() / threadCount * (threadNumber + 1) - 1,
-                        keys.size());
-
-                    for (auto keyIndex = nonExistentKeys.size() / threadCount * threadNumber;
-                        keyIndex < endKeyIndex;
-                        keyIndex++)
+                    if (keyIndex % 1000 == 0)
                     {
-                        if (keyIndex % 1000 == 0)
-                        {
-                            co_await *schedulers.ComputeScheduler;
-                        }
-
-                        auto myKey = nonExistentKeys[keyIndex];
-
-                        Perf2_running_items.fetch_add(1);
-
-                        StringKey key;
-                        key.set_value(myKey);
-                        StringValue expectedValue;
-                        expectedValue.set_value(myKey);
-
-                        ReadRequest readRequest;
-                        readRequest.Key = &key;
-                        readRequest.Index = index;
-
-                        auto readResult = co_await store->Read(
-                            readRequest
-                        );
-
-                        EXPECT_EQ(true, !readResult.Value);
-
-                        Perf2_running_items.fetch_sub(1);
+                        co_await *schedulers.ComputeScheduler;
                     }
 
-                }(threadNumber));
+                    auto myKey = nonExistentKeys[keyIndex];
+
+                    Perf2_running_items.fetch_add(1);
+
+                    StringKey key;
+                    key.set_value(myKey);
+                    StringValue expectedValue;
+                    expectedValue.set_value(myKey);
+
+                    ReadRequest readRequest;
+                    readRequest.Key = &key;
+                    readRequest.Index = index;
+
+                    auto readResult = co_await store->Read(
+                        readRequest
+                    );
+
+                    EXPECT_EQ(true, !readResult.Value);
+
+                    Perf2_running_items.fetch_sub(1);
+                }
+            };
+
+            cppcoro::async_scope asyncScopeRead;
+            m_perfScope = &asyncScopeRead;
+
+            for (int threadNumber = 0; threadNumber < threadCount; threadNumber++)
+            {
+                performanceTasks.push_back(readItemsLambda(threadNumber));
+                asyncScopeRead.spawn(performanceTasks.back());
             }
 
-            for (auto& task : tasks)
-            {
-                co_await task;
-            }
+            co_await asyncScopeRead.join();
 
             auto endReadTime = chrono::high_resolution_clock::now();
             auto readRuntimeMs = chrono::duration_cast<chrono::milliseconds>(endReadTime - startReadTime);
 
-            std::cout << "ProtoStoreTests read nonexistent runtime: " << readRuntimeMs.count() << "\r\n";
+            std::cout << "ProtoStoreTests read nonexistent runtime: " << readRuntimeMs.count() << "ms\r\n";
         };
 
         co_await readNonExistentLambda();
@@ -1534,7 +1543,7 @@ TEST_F(ProtoStoreTests, PerformanceTest(Perf2))
             auto checkpointEndTime = chrono::high_resolution_clock::now();
             auto checkpointRuntimeMs = chrono::duration_cast<chrono::milliseconds>(checkpointEndTime - checkpointStartTime);
 
-            std::cout << "ProtoStoreTests checkpoint runtime: " << checkpointRuntimeMs.count() << "\r\n";
+            std::cout << "ProtoStoreTests checkpoint runtime: " << checkpointRuntimeMs.count() << "ms\r\n";
         };
 
         co_await checkpointLambda();
