dc3602d998adb211d07025957751bac6ee17b5a5
Joshua Rowe <j.git@phantom.to>
2023-03-16 11:17:10 -0700

    Add make_shared_with_strong_reference Add value_factory

--- Phantom.System.Test/utility_test.cpp
+++ Phantom.System.Test/utility_test.cpp
@@ -35,4 +35,49 @@ TEST(copy_shared_test, makes_copy_of_reference_type)
     EXPECT_EQ(*copy, statistics);
 }
 
+TEST(value_factory_test, returns_convertible_object)
+{
+    auto valueFactory = value_factory([] { return "hello world"; });
+    std::string value = valueFactory;
+    EXPECT_EQ(value, "hello world");
+}
+
+TEST(make_shared_with_strong_reference_test, moves_lifetime_object_once_and_makes_result_without_copying_or_moving)
+{
+    lifetime_statistics lifetimeStatistics;
+    lifetime_statistics argumentStatistics;
+    
+    struct value
+    {
+        lifetime_tracker argumentTracker;
+
+        value(
+            lifetime_tracker&& arg
+        ) : argumentTracker(std::move(arg))
+        {}
+
+        // non-copyable, non-movable
+        value& operator=(value&&) = delete;
+    };
+
+    auto result = make_shared_with_strong_reference<value>(
+        lifetimeStatistics.tracker(),
+        argumentStatistics.tracker()
+    );
+
+    EXPECT_EQ(lifetimeStatistics.instance_count, 1);
+    EXPECT_EQ(lifetimeStatistics.move_construction_count, 1);
+    EXPECT_EQ(lifetimeStatistics.copy_construction_count, 0);
+
+    EXPECT_EQ(argumentStatistics.instance_count, 1);
+    EXPECT_EQ(argumentStatistics.move_construction_count, 1);
+    EXPECT_EQ(argumentStatistics.copy_construction_count, 0);
+
+    result.reset();
+    
+    EXPECT_EQ(lifetimeStatistics.instance_count, 0);
+    EXPECT_EQ(argumentStatistics.instance_count, 0);
+
+}
+
 }
--- Phantom.System/utility.h
+++ Phantom.System/utility.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <concepts>
+#include <functional>
 #include <memory>
 #include <span>
 #include "concepts.h"
@@ -46,6 +48,143 @@ auto copy_shared(
     T&& other)
 {
     return std::make_shared<std::decay_t<T>>(other);
+}
+
+template<
+    typename T,
+    typename Lambda = std::function<T()>
+>
+class value_factory
+{
+    Lambda m_lambda;
+
+public:
+    template<
+        std::convertible_to<T> Other,
+        std::convertible_to<Lambda> OtherLambda
+    >
+    value_factory(
+        value_factory<Other, OtherLambda>&& other
+    ) :
+        m_lambda(std::move(other.m_lambda))
+    {}
+
+    template<
+        std::convertible_to<T> Other,
+        std::convertible_to<Lambda> OtherLambda
+    >
+    value_factory(
+        const value_factory<Other, OtherLambda>& other
+    ) :
+        m_lambda(other.m_lambda)
+    {}
+
+    template<
+        std::convertible_to<T> Other,
+        std::convertible_to<Lambda> OtherLambda
+    >
+    value_factory& operator =(
+        value_factory<Other, OtherLambda>&& other
+    )
+    {
+        m_lambda = std::move(other.m_lambda);
+        return *this;
+    }
+
+    template<
+        std::convertible_to<T> Other,
+        std::convertible_to<Lambda> OtherLambda
+    >
+    value_factory& operator=(
+        const value_factory<Other, OtherLambda>& other
+    )
+    {
+        m_lambda = other.m_lambda;
+        return *this;
+    }
+
+    value_factory(
+        std::convertible_to<Lambda> auto lambda
+    ) 
+        noexcept(std::is_nothrow_constructible_v<Lambda, decltype(lambda)>)
+        : m_lambda{ std::forward<decltype(lambda)>(lambda) }
+    {}
+
+    auto operator()() const noexcept(noexcept(m_lambda()))
+    {
+        return m_lambda();
+    }
+
+    operator T() const noexcept(noexcept(m_lambda()))
+    {
+        return m_lambda();
+    }
+
+    template<
+        typename Value
+    >
+    requires std::convertible_to<T, Value>
+    operator Value() const noexcept(noexcept(Value(m_lambda())))
+    {
+        return Value(m_lambda());
+    }
+};
+
+template<
+    std::invocable Lambda
+> value_factory(Lambda) -> value_factory<std::invoke_result_t<std::decay_t<Lambda>>, std::decay_t<Lambda>>;
+
+template<typename T, typename... Args>
+auto make_forwarding_value_factory(
+    Args&&... args
+)
+{
+    return value_factory{[&] { return T{ std::forward<Args>(args)... }; }};
+}
+
+// Make an std::shared_ptr to a value that is constructed in-place,
+// and maintain a strong reference to another object as part of
+// the resulting shared_ptr.
+//
+// This is useful when the arguments' lifetimes are controlled by
+// the strongReference, and the argument lifetimes should exceed
+// the lifetime of the constructed object.
+template<
+    typename T, 
+    typename Reference,
+    typename ... Args>
+std::shared_ptr<T> make_shared_with_strong_reference(
+    Reference&& strongReference,
+    Args&&... args
+)
+{
+    using value_type = std::decay_t<T>;
+    using strong_reference_type = std::remove_reference_t<Reference>;
+
+    struct HolderType
+    {
+        strong_reference_type m_strongReference;
+        value_type m_value;
+
+        HolderType(
+            Reference&& strongReference,
+            auto&& value
+        ) :
+            m_strongReference{ std::forward<Reference>(strongReference) },
+            m_value { std::forward<decltype(value)>(value) }
+        {}
+    };
+
+    auto holder = std::make_shared<HolderType>(
+        std::forward<Reference>(strongReference),
+        make_forwarding_value_factory<value_type>(std::forward<decltype(args)>(args)...)
+    );
+
+    return std::shared_ptr<T>
+    {
+        std::move(holder),
+        & holder->m_value,
+    };
 }
 
 }
