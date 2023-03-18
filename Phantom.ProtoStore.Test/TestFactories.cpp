2d52cfc770a6eed0f6e3415f4e236ed60d92664c
Joshua Rowe <j.git@phantom.to>
2023-03-18 13:14:11 -0700

    Add AddRow to TestFactories

--- Phantom.ProtoStore.Test/TestFactories.cpp
+++ Phantom.ProtoStore.Test/TestFactories.cpp
@@ -5,6 +5,9 @@
 namespace Phantom::ProtoStore
 {
 
+std::atomic<uint64_t> testLocalTransactionId;
+std::atomic<uint64_t> testWriteId;
+
 task<std::shared_ptr<IIndexData>> MakeInMemoryIndex(
     IndexName indexName,
     const Schema& schema
@@ -42,6 +45,85 @@ task<std::shared_ptr<IIndexData>> MakeInMemoryIndex(
     );
 
     co_return index;
+}
+
+
+task<OperationResult<>> AddRow(
+    const std::shared_ptr<IIndexData>& index,
+    ProtoValue key,
+    ProtoValue value,
+    SequenceNumber writeSequenceNumber,
+    SequenceNumber readSequenceNumber
+)
+{
+    auto createLoggedRowWrite = [&](auto checkpointNumber) -> task<FlatMessage<FlatBuffers::LoggedRowWrite>>
+    {
+        ValueBuilder valueBuilder;
+
+        auto keyOffset = index->GetKeyComparer()->BuildDataValue(
+            valueBuilder,
+            key);
+
+        auto valueOffset = index->GetValueComparer()->BuildDataValue(
+            valueBuilder,
+            value);
+
+        auto loggedRowWriteOffset = FlatBuffers::CreateLoggedRowWrite(
+            valueBuilder.builder(),
+            index->GetIndexNumber(),
+            ToUint64(writeSequenceNumber),
+            checkpointNumber,
+            keyOffset,
+            valueOffset,
+            0,
+            testLocalTransactionId.fetch_add(1),
+            testWriteId.fetch_add(1)
+        );
+
+        valueBuilder.builder().Finish(loggedRowWriteOffset);
+
+        co_return FlatMessage<FlatBuffers::LoggedRowWrite>(
+            std::make_shared<flatbuffers::FlatBufferBuilder>(std::move(valueBuilder.builder())));
+    };
+
+    auto delayedTransactionOutcome = std::make_shared<DelayedMemoryTableTransactionOutcome>(
+        0
+    );
+
+    auto checkpointNumber = co_await index->AddRow(
+        readSequenceNumber,
+        createLoggedRowWrite,
+        delayedTransactionOutcome
+    );
+
+    if (!checkpointNumber)
+    {
+        co_return std::unexpected
+        {
+            FailedResult
+            {
+                make_error_code(ProtoStoreErrorCode::WriteConflict),
+            }
+        };
+    }
+
+    auto outcome = delayedTransactionOutcome->BeginCommit(
+        writeSequenceNumber);
+
+    delayedTransactionOutcome->Complete();
+
+    if (outcome.Outcome == TransactionOutcome::Committed)
+    {
+        co_return{};
+    }
+
+    co_return std::unexpected
+    {
+        FailedResult
+        {
+            make_error_code(ProtoStoreErrorCode::AbortedTransaction),
+        }
+    };
 }
 
 }
--- Phantom.ProtoStore.Test/TestFactories.h
+++ Phantom.ProtoStore.Test/TestFactories.h
@@ -1,11 +1,23 @@
 #include "Phantom.ProtoStore/src/Index.h"
+#include "Phantom.ProtoStore/src/ValueComparer.h"
 
 namespace Phantom::ProtoStore
 {
 
+extern std::atomic<uint64_t> testLocalTransactionId;
+extern std::atomic<uint64_t> testWriteId;
+
 task<std::shared_ptr<IIndexData>> MakeInMemoryIndex(
     IndexName indexName,
     const Schema& schema
+);
+
+task<OperationResult<>> AddRow(
+    const std::shared_ptr<IIndexData>& index,
+    ProtoValue key,
+    ProtoValue value,
+    SequenceNumber writeSequenceNumber = SequenceNumber::Earliest,
+    SequenceNumber readSequenceNumber = SequenceNumber::Latest
 );
 
 }
