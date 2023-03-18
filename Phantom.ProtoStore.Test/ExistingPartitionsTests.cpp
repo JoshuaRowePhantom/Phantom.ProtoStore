12a9448c06d087c50a2697fd4b80c67b905c8786
Joshua Rowe <j.git@phantom.to>
2023-03-18 13:23:30 -0700

    Add test ExistingPartitionsTests::BeginReplay_adds_preexisting_partitions

--- Phantom.ProtoStore.Test/ExistingPartitionsTests.cpp
+++ Phantom.ProtoStore.Test/ExistingPartitionsTests.cpp
@@ -1,4 +1,5 @@
 #include "StandardIncludes.h"
+#include "Phantom.ProtoStore/src/ExtentName.h"
 #include "Phantom.ProtoStore/src/ExistingPartitions.h"
 #include "Phantom.ProtoStore/src/Resources.h"
 #include "TestFactories.h"
@@ -24,11 +25,52 @@ public:
         existingPartitions = Phantom::ProtoStore::MakeExistingPartitions(
             partitionsIndex);
     }
+
+    task<> AddPartitionsRow(
+        IndexNumber indexNumber,
+        PartitionNumber partitionNumber
+    )
+    {
+        FlatBuffers::PartitionsKeyT key;
+        key.index_number = 5;
+        key.header_extent_name = copy_unique(*MakePartitionHeaderExtentName(
+            indexNumber,
+            partitionNumber,
+            1,
+            "index"
+        ).extent_name.AsIndexHeaderExtentName());
+
+        FlatBuffers::PartitionsValueT value;
+
+        auto result = co_await AddRow(
+            partitionsIndex,
+            &key,
+            &value);
+
+        EXPECT_EQ(true, result.has_value());
+    }
 };
 
 ASYNC_TEST_F(ExistingPartitionsTests, BeginReplay_adds_preexisting_partitions)
 {
-    co_return;
+    co_await AddPartitionsRow(
+        5,
+        1005);
+
+    co_await AddPartitionsRow(
+        6,
+        1006);
+
+    co_await AddPartitionsRow(
+        6,
+        1007);
+
+    co_await existingPartitions->BeginReplay();
+
+    EXPECT_EQ(true, co_await existingPartitions->DoesPartitionNumberExist(1005));
+    EXPECT_EQ(true, co_await existingPartitions->DoesPartitionNumberExist(1006));
+    EXPECT_EQ(true, co_await existingPartitions->DoesPartitionNumberExist(1007));
+    EXPECT_EQ(false, co_await existingPartitions->DoesPartitionNumberExist(1008));
 }
 
 
