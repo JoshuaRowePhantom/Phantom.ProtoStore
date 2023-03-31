039a15948b129f7788d9d898de7cdc6d1fcbdf37
Joshua Rowe <j.git@phantom.to>
2023-03-31 07:50:38 -0700

    Remove denormalized data from PartitionsValue in test

--- Phantom.ProtoStore.Test/IndexPartitionMergeGeneratorTests.cpp
+++ Phantom.ProtoStore.Test/IndexPartitionMergeGeneratorTests.cpp
@@ -19,74 +19,74 @@ public:
         mergeParameters_merges_2_maxLevel_3.set_maxlevel(3);
     }
 };
-//
-//ASYNC_TEST_F(IndexPartitionMergeGeneratorTests, Does_not_generate_merges_when_merges_per_level_is_not_exceeded)
-//{
-//    auto partitionsRows = JsonToFlatRows<FlatBuffers::PartitionsKey, FlatBuffers::PartitionsValue>(
-//        {
-//            // Level 1
-//            {
-//                "{ index_number: 5, header_extent_name : { index_extent_name : { partition_number : 1, index_number : 5, level : 1 } } }",
-//                "{ data_extent_name : { index_extent_name : { partition_number : 1, index_number : 5, level : 1 } }, level : 2, latest_checkpoint_number : 1 }",
-//            },
-//            // Level 2
-//            {
-//                "{ index_number: 5, header_extent_name : { index_extent_name : { partition_number : 1, index_number : 5, level : 2 } } }",
-//                "{ data_extent_name : { index_extent_name : { partition_number : 1, index_number : 5, level : 2 } }, level : 2, latest_checkpoint_number : 1 }",
-//            },
-//        });
-//
-//    auto existingMerges = JsonToFlatRows<FlatBuffers::MergesKey, FlatBuffers::MergesValue>(
-//        {}
-//    );
-//
-//    auto actualMergeCandidateRows = mergeGenerator.GetMergeCandidates(
-//        mergeParameters_merges_2_maxLevel_3,
-//        partitionsRows,
-//        existingMerges);
-//
-//    auto expectedMergeCandidateRows = JsonToFlatRows<FlatBuffers::MergesKey, FlatBuffers::MergesValue>(
-//        {}
-//    );
-//
-//    EXPECT_TRUE(RowListsAreEqual(
-//        expectedMergeCandidateRows,
-//        actualMergeCandidateRows)
-//    );
-//
-//    co_return;
-//}
 
-//
-//ASYNC_TEST_F(IndexPartitionMergeGeneratorTests, Does_generate_merges_when_merges_per_level_is_not_exceeded)
-//{
-//    auto partitionsRows = JsonToFlatRows<FlatBuffers::PartitionsKey, FlatBuffers::PartitionsValue>(
-//        {
-//            {
-//                "{ index_number: 5, header_extent_name : { index_extent_name : { partition_number : 1, index_number : 5, level : 1 } } }",
-//                "{ data_extent_name : { index_extent_name : { partition_number : 1, index_number : 5, level : 1 } }, level : 1, latest_checkpoint_number : 1 }",
-//            }
-//        });
-//
-//    auto existingMerges = JsonToFlatRows<FlatBuffers::MergesKey, FlatBuffers::MergesValue>(
-//        {}
-//    );
-//
-//    auto actualMergeCandidateRows = mergeGenerator.GetMergeCandidates(
-//        mergeParameters_merges_2_maxLevel_3,
-//        partitionsRows,
-//        existingMerges);
-//
-//    auto expectedMergeCandidateRows = JsonToFlatRows<FlatBuffers::MergesKey, FlatBuffers::MergesValue>(
-//        {}
-//    );
-//
-//    EXPECT_TRUE(RowListsAreEqual(
-//        expectedMergeCandidateRows,
-//        actualMergeCandidateRows)
-//    );
-//
-//    co_return;
-//}
+ASYNC_TEST_F(IndexPartitionMergeGeneratorTests, Does_not_generate_merges_when_merges_per_level_is_not_exceeded)
+{
+    auto partitionsRows = JsonToFlatRows<FlatBuffers::PartitionsKey, FlatBuffers::PartitionsValue>(
+        {
+            // Level 1
+            {
+                "{ index_number: 5, header_extent_name : { index_extent_name : { partition_number : 1, index_number : 5, level : 1 } } }",
+                "{ latest_checkpoint_number : 1 }",
+            },
+            // Level 2
+            {
+                "{ index_number: 5, header_extent_name : { index_extent_name : { partition_number : 1, index_number : 5, level : 2 } } }",
+                "{ latest_checkpoint_number : 1 }",
+            },
+        });
+
+    auto existingMerges = JsonToFlatRows<FlatBuffers::MergesKey, FlatBuffers::MergesValue>(
+        {}
+    );
+
+    auto actualMergeCandidateRows = mergeGenerator.GetMergeCandidates(
+        mergeParameters_merges_2_maxLevel_3,
+        partitionsRows,
+        existingMerges);
+
+    auto expectedMergeCandidateRows = JsonToFlatRows<FlatBuffers::MergesKey, FlatBuffers::MergesValue>(
+        {}
+    );
+
+    EXPECT_TRUE(RowListsAreEqual(
+        expectedMergeCandidateRows,
+        actualMergeCandidateRows)
+    );
+
+    co_return;
+}
+
+
+ASYNC_TEST_F(IndexPartitionMergeGeneratorTests, Does_generate_merges_when_merges_per_level_is_not_exceeded)
+{
+    auto partitionsRows = JsonToFlatRows<FlatBuffers::PartitionsKey, FlatBuffers::PartitionsValue>(
+        {
+            {
+                "{ index_number: 5, header_extent_name : { index_extent_name : { partition_number : 1, index_number : 5, level : 1 } } }",
+                "{ latest_checkpoint_number : 1 }",
+            }
+        });
+
+    auto existingMerges = JsonToFlatRows<FlatBuffers::MergesKey, FlatBuffers::MergesValue>(
+        {}
+    );
+
+    auto actualMergeCandidateRows = mergeGenerator.GetMergeCandidates(
+        mergeParameters_merges_2_maxLevel_3,
+        partitionsRows,
+        existingMerges);
+
+    auto expectedMergeCandidateRows = JsonToFlatRows<FlatBuffers::MergesKey, FlatBuffers::MergesValue>(
+        {}
+    );
+
+    EXPECT_TRUE(RowListsAreEqual(
+        expectedMergeCandidateRows,
+        actualMergeCandidateRows)
+    );
+
+    co_return;
+}
 
 }
